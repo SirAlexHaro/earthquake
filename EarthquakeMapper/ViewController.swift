1f81fe2f784235f41efad72f41cc2a3b588fdd43
siralexharo <alexaoh22@yahoo.com>
2017-07-19 19:45:33 -0700

    table displaying data

--- EarthquakeMapper/ViewController.swift
+++ EarthquakeMapper/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate{
+    
+    var earthquakes = ["Los Angeles","Mexico","Cabo San Lucas", "San Jose", "Gilroy", "Guasave", "Sinaloa", "Guamuchil"
+    ,"navojoa", "Mazatlan", "Culichi"
+    ]
+    
+    @IBOutlet weak var recentTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,7 +29,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        earthquakes.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
 }
-
+   
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return earthquakes.count
+    }
+    
+    // how should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        // set text label to the model that is corresponding to the row in array
+        cell.textLabel?.text = earthquakes[indexPath.row]
+        cell.detailTextLabel?.text = earthquakes[indexPath.row]
+        // return cell so that Table View knows what to render in each row
+        return cell
+    }
+}
--- EarthquakeMapper/searchViewController.swift
+++ EarthquakeMapper/searchViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class searchViewController: UIViewController {
 
     override func viewDidLoad() {
