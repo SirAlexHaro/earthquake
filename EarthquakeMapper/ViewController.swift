ed597a1b50c4182958fdb508feeaec72349745a5
siralexharo <alexaoh22@yahoo.com>
2017-07-19 17:24:27 -0700

    initial commit

---
+++ EarthquakeMapper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EarthquakeMapper
+//
+//  Created by Alejandro Haro on 7/19/17.
+//  Copyright © 2017 Alejandro Haro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
